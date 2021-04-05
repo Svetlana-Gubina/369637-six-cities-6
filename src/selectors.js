5e308b7953e4d230e4e3a23a1fa83f5c7d0f744d
Svetlana Terenteva <gubinas83@gmail.com>
2021-04-05 14:30:28 +0300

    feat: WelcomeScreen problem with test WIP

--- src/selectors.js
+++ src/selectors.js
@@ -2,11 +2,11 @@ import {NameSpace} from './store/root-reducer/root-reducer';
 import {createSelector} from 'reselect';
 import HotelsModel from './models/hotels-model';
 
-export const getHotelsData = (state) => state[NameSpace.DATA].hotelsList;
 export const getSortType = (state) => state[NameSpace.SORT_TYPE].activeSortType;
 export const getAuthorizationStatus = (state) => state[NameSpace.AUTH].authorizationStatus;
 export const getIsDataLoaded = (state) => state[NameSpace.DATA].isDataLoaded;
 export const getActiveCityItem = (state) => state[NameSpace.CITY].activeCityItem;
+export const getHotelsData = (state) => state[NameSpace.DATA].hotelsList;
 
 export const getParsedHotelsData = createSelector(
     [getHotelsData],
